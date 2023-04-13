49b6a113f57e52fcdbaaa074c4c70198cea1a6b5
koval-guk <ppp.superkoval@gmail.com>
2023-04-13 22:54:57 +0300

    add

---
+++ src/main/java/homework/web/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+package homework.web;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello, World!");
+        writer.close();
+    }
+}
